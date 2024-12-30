4f781e9904f5a34cb6d2971a521edefb89caa6cd
uo288090 <uo288090@uniovi.es>
2024-12-30 23:27:08 +0100

    loading the project

---
+++ src/test/java/com/chats/whattsapp/WhattsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chats.whattsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhattsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
