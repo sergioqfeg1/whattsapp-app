e46715d4ab0414c88a09c6898de66b0a6f9ecadc
uo288090 <uo288090@uniovi.es>
2024-12-31 18:28:51 +0100

    Adding equals to User

--- src/main/java/com/chats/whattsapp/model/User.java
+++ src/main/java/com/chats/whattsapp/model/User.java
@@ -67,4 +67,55 @@ public class User {
         this.profilePicture = profilePicture;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((fullName == null) ? 0 : fullName.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        result = prime * result + ((profilePicture == null) ? 0 : profilePicture.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (fullName == null) {
+            if (other.fullName != null)
+                return false;
+        } else if (!fullName.equals(other.fullName))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        if (profilePicture == null) {
+            if (other.profilePicture != null)
+                return false;
+        } else if (!profilePicture.equals(other.profilePicture))
+            return false;
+        return true;
+    }
+
+    
+
 }
